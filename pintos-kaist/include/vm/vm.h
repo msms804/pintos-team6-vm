697b016c1f47959d72e1598acdd6115a1be21c8d
msms804 <104570214+msms804@users.noreply.github.com>
2025-05-31 16:26:13 +0900

    make clean

--- pintos-kaist/include/vm/vm.h
+++ pintos-kaist/include/vm/vm.h
@@ -49,7 +49,7 @@ struct page {
 
 	/* Per-type data are binded into the union.
 	 * Each function automatically detects the current union */
-	union {
+	union { // 네가지 구조체 중 하나만 저장할 수 있음
 		struct uninit_page uninit;
 		struct anon_page anon;
 		struct file_page file;
